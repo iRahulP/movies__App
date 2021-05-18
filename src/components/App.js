adabfbf48bbadb7055e528c3ea213491327e3a1b
rahul patil <rahulpatil.m30@gmail.com>
2021-05-18 19:20:51 +0530

    partial components added

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,22 @@
+import { data } from "../data";
+import Navbar from './Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h3>Project Setup</h3>
+      <Navbar />
+      <div className="main">
+        <div className="tabs">
+          <div className="tab">Movies</div>
+          <div className="tab">Favourites</div>
+        </div>
+
+        <div className="list">
+          {data.map((movie) => {
+            <MovieCard movie={movie} />
+          })}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Navbar extends React.Component {
+    render () {
+        return (
+            <div className="nav">
+                <div className="search-container">
+                    <input />
+                    <buton id="btn">Search</buton>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Navbar;
+ 
