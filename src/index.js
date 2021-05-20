501e70d834e4cac6c6c4d5c665f65f061c8028c7
iRahulP <rahulpatil.m30@gmail.com>
2021-05-20 21:04:00 +0530

    added redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import './index.css';
 import App from './components/App';
+import movies from './reducers';
+
+const store = createStore(movies);
+console.log('store', store);
+console.log('state', store.getState());
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
 
