9b451e8ca3b537e47e641e85dc8aef506a9b975c
iRahulP <rahulpatil.m30@gmail.com>
2021-05-21 14:37:18 +0530

    updated styles

--- src/actions/index.js
+++ src/actions/index.js
@@ -10,6 +10,7 @@ export const ADD_MOVIES = 'ADD_MOVIES';
 export const ADD_TO_FAVOURITES = 'ADD_TO_FAVOURITES';
 export const REMOVE_FROM_FAVOURITES = 'REMOVE_FROM_FAVOURITES';
 export const SET_SHOW_FAVOURITES = 'SET_SHOW_FAVOURITES';
+export const ADD_MOVIE_TO_LIST = 'ADD_MOVIE_TO_LIST';
 
 //action creators
 export function addMovies(movies) {
@@ -38,4 +39,24 @@ export function setShowFavourites(val) {
         type: SET_SHOW_FAVOURITES,
         val: val
     }
+}
+
+export function addMovieToList(movie) {
+    return {
+        type: ADD_MOVIE_TO_LIST,
+        movie
+    }
+}
+
+export function handleMovieSearch(movie) {
+
+    const url = `http://www.omdbapi.com/?apikey=c916a477&t=${movie}`;
+
+    fetch(url)
+        .then(response => console.log(response));
+
+    // return {
+    //     type: ADD_MOVIE_TO_LIST,
+    //     movie
+    // }
 }
--- src/index.js
+++ src/index.js
@@ -8,14 +8,20 @@ import rootReducer from './reducers';
 
 //curried form of function :: {logger(obj, next, action)}
 //logger(obj)(next)(action)
-const logger = function ({ dispatch, getState }) {
-  return function (next) {
-    return function (action) {
-      //middleware code
-      console.log("ACTION_TYPE : ", action.type);
-      next(action);
-    }
-  }
+// const logger = function ({ dispatch, getState }) {
+//   return function (next) {
+//     return function (action) {
+//       //middleware code
+//       console.log("ACTION_TYPE : ", action.type);
+//       next(action);
+//     }
+//   }
+// }
+
+const logger = ({ dispatch, getState }) => (next) => (action) => {
+  //logger code
+  console.log("ACTION_TYPE : ", action.type);
+  next(action);
 }
 
 const store = createStore(rootReducer, applyMiddleware(logger));
