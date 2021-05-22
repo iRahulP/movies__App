4ef7b71820dcf03df819f860a717533950ddfd98
iRahulP <rahulpatil.m30@gmail.com>
2021-05-22 12:40:41 +0530

    added redux-thunk

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
-
+import thunk from 'redux-thunk'
 import './index.css';
 import App from './components/App';
 import rootReducer from './reducers';
@@ -21,17 +21,19 @@ import rootReducer from './reducers';
 const logger = ({ dispatch, getState }) => (next) => (action) => {
   //logger code
   // console.log("ACTION_TYPE : ", action.type);
-  next(action);
-}
-
-const thunk = ({ dispatch, getState }) => (next) => (action) => {
-  //logger code
-  if (typeof action == 'function') {
-    action(dispatch);
-    return;
+  if (typeof action !== 'function') {
+    console.log('Action Type', action.type);
   }
   next(action);
 }
+// const thunk = ({ dispatch, getState }) => (next) => (action) => {
+//   //logger code
+//   if (typeof action == 'function') {
+//     action(dispatch);
+//     return;
+//   }
+//   next(action);
+// }
 
 const store = createStore(rootReducer, applyMiddleware(logger, thunk));
 console.log('store', store);
